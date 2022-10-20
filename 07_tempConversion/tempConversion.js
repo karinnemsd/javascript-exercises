eef92ac21140fbb12355d8415dbc15acbb9fe96f
karinnemsd <karinnemsd@gmail.com>
2022-10-20 18:00:03 -0300

    do exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let tempConv = (temp - 32) * (5 / 9);
+  let tempConvRounded = Math.round(tempConv * 10) / 10;
 
+  return tempConvRounded;
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let tempConv = (temp * (9 / 5)) + 32;
+  let tempConvRounded = Math.round(tempConv * 10) / 10;
 
+  return tempConvRounded;
 };
 
 // Do not edit below this line
